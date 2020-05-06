879f7a3b567aa70de50f28f8e8cf7ee847d5cd13
onestarYX <40019226+onestarYX@users.noreply.github.com>
2020-05-05 17:06:18 -0700

    Error in dense layer output: OOM

--- prototype.py
+++ prototype.py
@@ -51,28 +51,32 @@ print(concat_input.shape)
 
 conv_layer_1 = layers.Conv3D(filters=1, kernel_size=(3, 3, 3), input_shape=[*vol_size, 2], activation='relu')(concat_input)
 conv_layer_2 = layers.Conv3D(filters=1, kernel_size=(5, 5, 5), activation="relu")(conv_layer_1)
+conv_layer_3 = layers.Conv3D(filters=1, kernel_size=(20, 20, 20), activation="relu")(conv_layer_2)
+dense_layer_1 = layers.Flatten()(conv_layer_3)
+dense_layer_2 = layers.Dense(58982400, activation="relu")(dense_layer_1)
+# reshaped_output = layers.Reshape((*vol_size, 3))(dense_layer_2)
 
-random_flow_list = []
-for i in range(vol_size[0] * vol_size[1] * vol_size[2] * 3):
-    random_flow_list.append(random.uniform(20, 25))
-
-random_flow = np.array(random_flow_list)
-random_flow = np.reshape(random_flow, (*vol_size, 3))
-print(random_flow.shape)
-
-
-xx = np.arange(vol_size[1])
-yy = np.arange(vol_size[0])
-zz = np.arange(vol_size[2])
-grid = np.rollaxis(np.array(np.meshgrid(xx, yy, zz)), 0, 4)
-print(grid[1,0,2])
-
-sample = random_flow+grid
-sample = np.stack((sample[:, :, :, 1], sample[:, :, :, 0], sample[:, :, :, 2]), 3)
-warped = interpolate.interpn((yy, xx, zz), img1, sample, method='nearest', bounds_error=False, fill_value=0)
-print(warped.shape)
-plt.imshow(warped[:,:,100])
-plt.show()
+# random_flow_list = []
+# for i in range(vol_size[0] * vol_size[1] * vol_size[2] * 3):
+#     random_flow_list.append(random.uniform(20, 25))
+#
+# random_flow = np.array(random_flow_list)
+# random_flow = np.reshape(random_flow, (*vol_size, 3))
+# print(random_flow.shape)
+#
+#
+# xx = np.arange(vol_size[1])
+# yy = np.arange(vol_size[0])
+# zz = np.arange(vol_size[2])
+# grid = np.rollaxis(np.array(np.meshgrid(xx, yy, zz)), 0, 4)
+# print(grid[1,0,2])
+#
+# sample = random_flow+grid
+# sample = np.stack((sample[:, :, :, 1], sample[:, :, :, 0], sample[:, :, :, 2]), 3)
+# warped = interpolate.interpn((yy, xx, zz), img1, sample, method='nearest', bounds_error=False, fill_value=0)
+# print(warped.shape)
+# plt.imshow(warped[:,:,100])
+# plt.show()
 
 # Write the img array to a txt file (slow and huge, caution!)
 # file = open("./data/NetworkInput/sampleKneeInput.txt", "w")
--- resize.py
+++ resize.py
@@ -1,8 +1,10 @@
 
-import cv2 
+import cv2
 import numpy as np
 import nibabel as nib
+from matplotlib import pyplot as plt
 
+path = './data/'
 img1_path = path + 'knee1FS time1 337172.nii.gz'
 img2_path = path + 'knee1FS time2 338366.nii.gz'
 img3_path = path + 'Knee2NFS Time1 20090305.nii.gz'
@@ -31,7 +33,7 @@ for i in range(len(atlas[0])):
         su = np.append(atlas[0][i][j], np.zeros((32,1)))
         sub.append(su)
     atlas_224.append(sub)
-    
+
 X_vol_224 = []
 for i in range(len(X_vol[0])):
     sub = []
@@ -44,16 +46,20 @@ atlas_224 = np.array(atlas_224)
 X_vol_224 = np.array(X_vol_224)
 print((atlas_224).shape)
 print((X_vol_224).shape)
+# plt.imshow(atlas_224[:,:,191])
+# plt.show()
 
 atlas_224_resized = atlas_224.copy()
 stretch_near = cv2.resize(atlas_224_resized, (192, 160), interpolation = cv2.INTER_NEAREST)
 Titles ="Interpolation Nearest"
-atlas_224_resized = stretch_near 
+atlas_224_resized = stretch_near
+plt.imshow(atlas_224_resized[:,:,200])
+plt.show()
 
 X_vol_224_resized = X_vol_224.copy()
 stretch_near = cv2.resize(X_vol_224_resized, (192, 160), interpolation = cv2.INTER_NEAREST)
 Titles ="Interpolation Nearest"
-X_vol_224_resized = stretch_near 
+X_vol_224_resized = stretch_near
 
 X_vol_224_resized = np.expand_dims(np.expand_dims(X_vol_224_resized, 0), -1)
 atlas_224_resized = np.expand_dims(np.expand_dims(atlas_224_resized, 0), -1)
